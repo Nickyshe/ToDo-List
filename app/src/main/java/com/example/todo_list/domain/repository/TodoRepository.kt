e3adadb042532aeeb18b794d2ac8f18237a61ea3
Nickyshe <judypearls200@gmail.com>
2023-12-20 13:50:06 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/todo_list/domain/repository/TodoRepository.kt
@@ -0,0 +1,4 @@
+package com.example.todo_list.domain.repository
+
+class TodoRepository {
+}
---
+++ app/src/main/java/com/example/todo_list/ui/presentation/TodoScreen.kt
@@ -0,0 +1,47 @@
+package com.example.todo_list.ui.presentation
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.todo_list.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun TodoScreen() {
+
+    Text(
+        text = "TODO APP",
+        style = TextStyle(
+            fontSize = 24.sp,
+                fontFamily = FontFamily(Font(R.font.jost_bold)),
+             fontWeight = FontWeight(600),
+            color = Color(0xFFFFFFFF),
+        )
+    )
+
+
+
+}
+
+@Preview
+@Composable
+fun TodoScreenPreview() {
+    TodoScreen()
+
+}
