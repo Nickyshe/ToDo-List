dbf90951e81e4816dda38679890f2416d4e215a4
Nickyshe <judypearls200@gmail.com>
2023-12-21 15:59:20 +0100

    hilt version changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,8 +88,8 @@ dependencies {
 
 
     //Dagger-Hilt dependencies
-    implementation("com.google.dagger:hilt-android:2.44.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.44.1")
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
 
